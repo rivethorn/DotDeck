cda69076fd4a23aa98feffcd0ed0ae1f19a2bd7a
Hassan Ghasemi <hassanak813@outlook.com>
2025-08-21 01:45:45 +0330

    just taking life simpler

--- internal/utils.go
+++ internal/utils.go
@@ -3,7 +3,7 @@ package internal
 import "fmt"
 
 // LogVerbose is a helper function used to give the user detailed output when verbose flag is used.
-func LogVerbose(on bool, format string, a ...interface{}) {
+func LogVerbose(on bool, format string, a ...any) {
 	if on {
 		fmt.Printf(format+"\n", a...)
 	}
